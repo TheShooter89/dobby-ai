cf33a039f6809c03d1b4c971170a9f4b9bec0afa
tanque <paoletti.francesco@gmail.com>
2024-09-16 17:58:55 +0200

    add dobby svg image and soon-to-be typewriter text

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
+import dobby_logo from "/dobby.svg";
 
 import { h, FC, BaseProps } from "tsx-dom";
 
@@ -9,7 +10,25 @@ import { Navbar } from "./navbar.tsx";
 
 interface ButtonProps extends BaseProps { }
 
-const App: FC<ButtonProps> = () => (<h1>app</h1>)
+const App: FC<ButtonProps> = () => (
+  <section class="section container is-fluid is-fullheight">
+    <div class="container">
+      <div class="fixed-grid has-2-cols">
+        <div class="grid dobby-grid">
+          <div class="cell dobby-grid-logo-cell">
+            <figure class="image is-1by1 dobby-logo">
+              <img src={dobby_logo} />
+            </figure>
+          </div>
+
+          <div class="cell">
+            <p>Very long text to be rendered with typewriter effect</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
 
 const body = document.querySelector<HTMLDivElement>('body');
 body?.insertBefore(<App />, body.firstChild);
