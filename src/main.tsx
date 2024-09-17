db9499549ee6827f8145bf778611a0c90efeebf1
tanque <paoletti.francesco@gmail.com>
2024-09-17 12:39:43 +0200

    remove old code and import components (and styles) from their files

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,19 @@
 import './style.css'
+import './components/navbar.css'
+import './components/app.css'
+import './components/dobby.css'
+import './components/cloche-button.css'
+import './components/interaction-box.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import dobby_logo from "/dobby.svg";
 
 import { h, FC, BaseProps } from "tsx-dom";
 
-import { setupCounter } from './counter.ts'
-import { Navbar } from "./navbar.tsx";
-
-interface ButtonProps extends BaseProps { }
-
-const App: FC<ButtonProps> = () => (
-  <section class="section container is-fluid is-fullheight">
-    <div class="container">
-      <div class="fixed-grid has-2-cols">
-        <div class="grid dobby-grid">
-          <div class="cell dobby-grid-logo-cell">
-            <figure class="image is-1by1 dobby-logo">
-              <img src={dobby_logo} />
-            </figure>
-          </div>
-
-          <div class="cell">
-            <p>Very long text to be rendered with typewriter effect</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-)
+import { Navbar } from "./components/navbar.tsx";
+import { App } from "./components/app.tsx";
 
 const body = document.querySelector<HTMLDivElement>('body');
+
 body?.insertBefore(<App />, body.firstChild);
 body?.insertBefore(<Navbar />, body.firstChild);
