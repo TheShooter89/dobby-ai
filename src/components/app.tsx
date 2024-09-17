18980c5c732a64a0364c31e9fd97218624ef6f86
tanque <paoletti.francesco@gmail.com>
2024-09-17 12:37:45 +0200

    move App component into its own file and separate styles

---
+++ src/components/app.tsx
@@ -0,0 +1,23 @@
+import { h, FC, BaseProps } from "tsx-dom";
+
+import { DobbyAI } from "./dobby.tsx";
+import { ClocheButton } from "./cloche-button.tsx";
+import { InteractionBox } from "./interaction-box.tsx";
+
+export const App: FC<BaseProps> = () => (
+  <section class="section container is-fluid is-fullheight dobby-section">
+    <div class="columns">
+      <div class="column">
+        <DobbyAI />
+      </div>
+
+      <div class="column">
+        <ClocheButton />
+      </div>
+
+      <div class="column">
+        <InteractionBox />
+      </div>
+    </div>
+  </section>
+);
