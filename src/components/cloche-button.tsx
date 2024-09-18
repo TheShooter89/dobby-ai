3d658e16bca186e03cd5fe3badd83663cd7aca1c
tanque <paoletti.francesco@gmail.com>
2024-09-18 11:33:21 +0200

    add click action to ClocheButton

--- src/components/cloche-button.tsx
+++ src/components/cloche-button.tsx
@@ -2,8 +2,14 @@ import { h, FC, BaseProps } from "tsx-dom";
 
 interface ClocheButtonProps extends BaseProps {
   text?: string
+  onclick?(event: Event): any
 }
 
-export const ClocheButton: FC<ClocheButtonProps> = ({ text = "ClocheButton" }: ClocheButtonProps) => (
-  <button id="cloche-button" class="button is-primary oswald-font cloche-button cloche-button-hidden">{text}</button>
+function default_click_action(this: HTMLElement, event: Event) {
+  event.preventDefault();
+  console.log('default click action!');
+}
+
+export const ClocheButton: FC<ClocheButtonProps> = ({ text = "ClocheButton", onclick = default_click_action }: ClocheButtonProps) => (
+  <button id="cloche-button" onClick={onclick} class="button is-primary oswald-font cloche-button cloche-button-hidden">{text}</button>
 );
