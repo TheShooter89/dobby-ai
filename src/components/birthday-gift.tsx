626d77aa981668f93f97cb97bb2543e3b7be5b65
tanque <paoletti.francesco@gmail.com>
2024-09-24 12:26:57 +0200

    fix vertical spacing

--- src/components/birthday-gift.tsx
+++ src/components/birthday-gift.tsx
@@ -3,9 +3,9 @@ import { h, FC, BaseProps } from "tsx-dom";
 import dobby_logo from "/dobby.svg";
 
 export const BirthdayGift: FC<BaseProps> = () => (
-  <section>
-    <h2 class="title is-size-3">Tanti Auguri</h2>
-    <p>buon compleanno Erica</p>
+  <section class="mt-5">
+    <h2 class="title is-size-2 is-size-3-mobile mb-4 has-text-centered">Tanti Auguri</h2>
+    <p class="subtitle is-size-4 is-size-5-mobile oswald-font mb-5 has-text-centered">buon compleanno Erica</p>
     <img src={dobby_logo} class="image" />
   </section>
 );
