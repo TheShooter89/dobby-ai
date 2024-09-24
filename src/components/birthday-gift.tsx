4037cd00411dde06e91cfaee14827da805f0e6f8
tanque <paoletti.francesco@gmail.com>
2024-09-24 13:05:31 +0200

    add birthday gift image

--- src/components/birthday-gift.tsx
+++ src/components/birthday-gift.tsx
@@ -2,10 +2,12 @@ import { h, FC, BaseProps } from "tsx-dom";
 
 import dobby_logo from "/dobby.svg";
 
+import birthday_gift_image_1 from "/birthday_gift_image_1.jpg";
+
 export const BirthdayGift: FC<BaseProps> = () => (
   <section class="mt-5">
     <h2 class="title is-size-2 is-size-3-mobile mb-4 has-text-centered">Tanti Auguri</h2>
     <p class="subtitle is-size-4 is-size-5-mobile oswald-font mb-5 has-text-centered">buon compleanno Erica</p>
-    <img src={dobby_logo} class="image" />
+    <img src={birthday_gift_image_1} class="image" />
   </section>
 );
